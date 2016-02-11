e7ae1332539495efecd895ce37976e8d2e2cab3d
PEMapModder <pemapmodder1970@gmail.com>
2016-02-11 10:32:43 -0500

    Added pull request cloning

--- run.sh
+++ run.sh
@@ -5,15 +5,39 @@ printDate(){
 }
 
 while true; do
-	mkdir artifacts 2>/dev/null
+	mkdir -p artifacts/pr 2>/dev/null
 	rm -rf clone
 	printDate; echo Checking for updates...
 	COMMITS=$(wget -O - --header="Authorization: bearer $(cat /GITHUB_TOKEN)" --header="User-Agent: PocketMine-MP-Compiler pmt.mcpe.me" https://api.github.com/repos/PocketMine/PocketMine-MP/events 2>/dev/null | php scanBranches.php)
+	echo "$COMMITS"
 	while IFS= read -r line; do
 		if [ -z "$line" ]; then
 			break
 		fi
 		BRANCH=$(echo $line | tr ":" "\n" | head -n 1)
+		if [ "$BRANCH" == "pr" ]; then
+			PR_ID=$(echo $line | tr ":" "\n" | sed '2!d')
+			REMOTE=$(echo $line | tr ":" "\n" | sed '3!d' | tr ";" ":")
+			SHA=$(echo $line | tr ":" "\n" | sed '4!d')
+			printDate; echo Building pull request...
+			echo "==="
+			echo "Pull Request: #$PR_ID"
+			echo "Remote:       $REMOTE"
+			echo "SHA:          $SHA"
+			mkdir clone
+			cd clone
+			printDate; echo Cloning...
+			(git clone "$REMOTE" . && git submodule update --init --recursive) 2>/dev/null >/dev/null
+			printDate; echo Checking out this version...
+			echo "git checkout $SHA"
+			((git checkout "$SHA" && git submodule update) 3>&1 1>&2- 2>&3-) | tail -n 1
+			printDate; echo Compiling...
+			PHAR_OUT=$(php ../compile.php --out "../artifacts/pr/$PR_ID".phar --from "./")
+			printDate; echo $PHAR_OUT
+			cd ..
+			rm -rf clone
+			continue
+		fi
 		SHA=$(echo $line | tr ":" "\n" | tail -n 1)
 		printDate; echo Creating new version...
 		echo "==="
--- scanBranches.php
+++ scanBranches.php
@@ -2,20 +2,48 @@
 
 $last = json_decode(file_get_contents("last.json"));
 $data = array_filter(json_decode(file_get_contents("php://stdin")), function($ev) use($last){
-	return ((int) $ev->id) > $last->lastEvent and $ev->type === "PushEvent";
+	if(intval($ev->id) <= $last->lastEvent) return false;
+	if($ev->type === "PushEvent"){
+		return true;
+	}
+	if($ev->type === "PullRequestEvent"){
+		if($ev->payload->action === "opened"){
+			return true;
+		}
+	}
+	return false;
 });
 
 $branches = [];
 $max = $last->lastEvent;
 foreach($data as $event){
 	$max = (int) max($max, (int) $event->id);
-	$branch = explode("/", $event->payload->ref)[2];
-	$sha = $event->payload->head;
-	if(!isset($branches[$branch])) $branches[$branch] = [
-		"sha" => $sha,
-		"time" => time(),
-		"trigger" => $event->actor->login
-	];
+	if($event->type === "PushEvent"){
+		$branch = explode("/", $event->payload->ref)[2];
+		$sha = $event->payload->head;
+		if(!isset($branches[$branch])) $branches[$branch] = [
+			"sha" => $sha,
+			"time" => time(),
+			"trigger" => $event->actor->login,
+			"title" => $event->payload->commits[0]->message
+		];
+	}elseif($event->type === "PullRequestEvent"){
+		$head = $event->payload->pull_request->head;
+		$ref = $head->ref;
+		$sha = $head->sha;
+		$id = $event->payload->pull_request->number;
+		if(!isset($branches["pr:" . $id])){
+			$branches["pr:" . $id] = [
+				"sha" => $sha,
+				"time" => time(),
+				"trigger" => $event->actor->login,
+				"title" => $event->payload->pull_request->title,
+				"remote" => $head->repo->clone_url,
+				"id" => $id,
+				"ref" => $ref
+			];
+		}
+	}
 }
 
 $last->lastEvent = $max;
@@ -26,6 +54,8 @@ foreach($branches as $branch => $data){
 file_put_contents("last.json", json_encode($last, JSON_PRETTY_PRINT));
 foreach($branches as $branch => $data){
 	$sha = $data["sha"];
-	echo "$branch:$sha", PHP_EOL;
+	if(isset($data["remote"])) echo "pr:" . $data["id"] . ":" . str_replace(":", ";", $data["remote"]) . ":$sha";
+	else echo "$branch:$sha";
+	echo PHP_EOL;
 }
 
