2d585e1da4f769cbd08a7e666708e3e7f64d1324
PEMapModder <pemapmodder1970@gmail.com>
2016-02-28 10:06:30 -0500

    Updates

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ printDate(){
 
 while true; do
 	mkdir -p artifacts/pr 2>/dev/null
-	rm -rf clone
 	printDate; echo Checking for updates...
 	COMMITS=$(wget -O - --header="Authorization: bearer $(cat /GITHUB_TOKEN)" --header="User-Agent: PocketMine-MP-Compiler pmt.mcpe.me" https://api.github.com/repos/PocketMine/PocketMine-MP/events 2>/dev/null | php scanBranches.php)
 	while IFS= read -r line; do
@@ -23,20 +22,21 @@ while true; do
 			echo "Pull Request: #$PR_ID"
 			echo "Remote:       $REMOTE"
 			echo "SHA:          $SHA"
+			rm -rf clone 2>/dev/null
 			mkdir clone
 			cd clone
 			printDate; echo Cloning...
 			(git clone "$REMOTE" . && git submodule update --init --recursive) 2>/dev/null >/dev/null
 			printDate; echo Checking out this version...
 			((git checkout "$SHA" && git submodule update) 3>&1 1>&2- 2>&3-) | tail -n 1
-			printDate; echo Optimizing...
+			printDate; echo Preprocessing...
 			php /PreProcessor/PreProcessor.php --path . --multisize | tr "\n" "\r"
+			printDate; echo Optimizing...
 			php /PreProcessor/CodeOptimizer.php --path . | tr "\n" "\r"
 			printDate; echo Compiling...
 			PHAR_OUT=$(php ../compile.php --out "../artifacts/pr/$PR_ID".phar --from "./")
 			printDate; echo $PHAR_OUT
 			cd ..
-			rm -rf clone
 			continue
 		fi
 		SHA=$(echo $line | tr ":" "\n" | tail -n 1)
@@ -44,17 +44,21 @@ while true; do
 		echo "==="
 		echo "Branch: $BRANCH"
 		echo "SHA:    $SHA"
+		rm -rf clone 2>/dev/null
 		mkdir clone
 		cd clone
 		printDate; echo Cloning...
 		(git clone https://github.com/PocketMine/PocketMine-MP.git . && git submodule update --init --recursive) 2>/dev/null >/dev/null
 		printDate; echo Checking out this version...
 		((git checkout "$SHA" && git submodule update) >/dev/null 2>&1) | tail -n 1
+		printDate; echo Preprocessing...
+		php /PreProcessor/PreProcessor.php --path . --multisize | tr "\n" "\r"
+		printDate; echo Optimizing...
+		php /PreProcessor/CodeOptimizer.php --path . | tr "\n" "\r"
 		printDate; echo Compiling...
 		PHAR_OUT=$(php ../compile.php --out "../artifacts/$BRANCH".phar --from "./")
 		printDate; echo $PHAR_OUT
 		cd ..
-		rm -rf clone
 	done <<< "$COMMITS"
 	printDate; echo Next check will be continued 5 minutes later
 	sleep 300
